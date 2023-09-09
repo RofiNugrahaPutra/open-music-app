15bd4a50d74e2308b44e89f3fead6bb219719fb6
Rofi Nugraha Putra <rofinugrahaputra@gmail.com>
2023-09-09 12:56:34 +0700

    Add .max() to title, performer, genre in Joi's validation schema

--- src/validators/songs/schema.js
+++ src/validators/songs/schema.js
@@ -3,10 +3,10 @@ const Joi = require('joi');
 const currentYear = new Date().getFullYear();
 
 const SongPayloadSchema = Joi.object({
-  title: Joi.string().required(),
+  title: Joi.string().max(255).required(),
   year: Joi.number().min(1900).max(currentYear).required(),
-  performer: Joi.string().required(),
-  genre: Joi.string().required(),
+  performer: Joi.string().max(255).required(),
+  genre: Joi.string().max(255).required(),
   duration: Joi.number(),
   albumId: Joi.string(),
 });
