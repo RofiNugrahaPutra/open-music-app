b400c89353027271d606974d8746ad52b730daf1
Rofi Nugraha Putra <rofinugrahaputra@gmail.com>
2023-10-22 21:33:41 +0700

    delete error handling in handler

--- src/api/exports/handler.js
+++ src/api/exports/handler.js
@@ -11,51 +11,32 @@ class ExportHandler {
   }
 
   async postExportPlaylistsHandler(req, res) {
-    try {
-      this._exportValidator.validateExportPlaylistPayload(req.payload);
-      const { playlistId } = req.params;
-      const { id: credentialId } = req.auth.credentials;
+    this._exportValidator.validateExportPlaylistPayload(req.payload);
+    const { playlistId } = req.params;
+    const { id: credentialId } = req.auth.credentials;
 
-      await this._playlistService.verifyPlaylistAccess(playlistId, credentialId);
+    await this._playlistService.verifyPlaylistAccess(playlistId, credentialId);
 
-      const playlistById = await this._playlistService.getPlaylistById(playlistId);
-      const songs = await this._playlistSongService.getSongsFromPlaylistId(playlistId);
+    const playlistById = await this._playlistService.getPlaylistById(playlistId);
+    const songs = await this._playlistSongService.getSongsFromPlaylistId(playlistId);
 
-      const message = {
-        playlistTarget: {
-          playlist: {
-            id: playlistById.id,
-            name: playlistById.name,
-            songs,
-          },
+    const message = {
+      playlistTarget: {
+        playlist: {
+          id: playlistById.id,
+          name: playlistById.name,
+          songs,
         },
-        targetEmail: req.payload.targetEmail,
-      };
+      },
+      targetEmail: req.payload.targetEmail,
+    };
 
-      await this._exportService.sendMessage('export:playlist', JSON.stringify(message));
+    await this._exportService.sendMessage('export:playlist', JSON.stringify(message));
 
-      return res.response({
-        status: 'success',
-        message: 'Permintaan Anda sedang kami proses',
-      }).code(201);
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = res.response({
-          status: 'fail',
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-      // Server ERROR!
-      const response = res.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    return res.response({
+      status: 'success',
+      message: 'Permintaan Anda sedang kami proses',
+    }).code(201);
   }
 }
 
--- src/api/uploads/handler.js
+++ src/api/uploads/handler.js
@@ -8,40 +8,21 @@ class UploadHandler {
   }
 
   async postUploadAlbumCoverHandler(req, res) {
-    try {
-      const { cover } = req.payload;
-      const { id } = req.params;
+    const { cover } = req.payload;
+    const { id } = req.params;
 
-      this._uploadValidator.validateImageHeaders(cover.hapi.headers);
+    this._uploadValidator.validateImageHeaders(cover.hapi.headers);
 
-      const filename = await this._uploadService.writeFile(cover, cover.hapi);
-      const coverUrl = `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`;
+    const filename = await this._uploadService.writeFile(cover, cover.hapi);
+    const coverUrl = `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`;
 
-      const { name, year } = await this._albumService.getAlbumById(id);
-      await this._albumService.editAlbumById(id, { name, year, coverUrl });
+    const { name, year } = await this._albumService.getAlbumById(id);
+    await this._albumService.editAlbumById(id, { name, year, coverUrl });
 
-      return res.response({
-        status: 'success',
-        message: 'Sampul berhasil diunggah',
-      }).code(201);
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = res.response({
-          status: 'fail',
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-      // Server ERROR!
-      const response = res.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    return res.response({
+      status: 'success',
+      message: 'Sampul berhasil diunggah',
+    }).code(201);
   }
 }
 
